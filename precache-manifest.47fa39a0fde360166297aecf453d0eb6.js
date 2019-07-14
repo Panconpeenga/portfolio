79971390374ca5c912c6137e0ff94a335df1e58a
Panconpeenga <imsoto@icloud.com>
2019-07-14 04:45:49 -0400

    Updates

---
+++ precache-manifest.47fa39a0fde360166297aecf453d0eb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336a3a30b782c14f2802b4d7c4152ffc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7e9f8c5bca412abd3828",
+    "url": "/portfolio/static/css/2.90271e63.chunk.css"
+  },
+  {
+    "revision": "a41e1def3a3f06394200",
+    "url": "/portfolio/static/css/main.a159ce7c.chunk.css"
+  },
+  {
+    "revision": "7e9f8c5bca412abd3828",
+    "url": "/portfolio/static/js/2.b790e4c9.chunk.js"
+  },
+  {
+    "revision": "a41e1def3a3f06394200",
+    "url": "/portfolio/static/js/main.926ba5a1.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "3e8b75df1d338f57ca98143aa8bc650a",
+    "url": "/portfolio/static/media/icon.3e8b75df.png"
+  }
+]);
